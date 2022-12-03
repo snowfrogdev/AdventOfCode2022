d91531919ca54e651818f55e717007f12b89fb7d
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2022-12-03 17:15:35 -0500

    Completed 03-RucksackReorganization-Part 2

--- solutions/03-RucksackReorganization/RucksackReorganization.cs
+++ solutions/03-RucksackReorganization/RucksackReorganization.cs
@@ -5,8 +5,8 @@ public static class RucksackReorganization
   public static char FindCommonLetterInCompartments(string rucksack)
   {
     int mid = (rucksack.Length + 1) / 2;
-    var compartments = (rucksack[..mid], rucksack[mid..]);	
-    return compartments.Item1.Intersect(compartments.Item2).Single();	
+    var compartments = (rucksack[..mid], rucksack[mid..]);
+    return compartments.Item1.Intersect(compartments.Item2).Single();
   }
 
   public static int ConvertLetterToPriority(char letter)
@@ -23,6 +23,21 @@ public static class RucksackReorganization
   {
     return rucksacks
       .Select(FindCommonLetterInCompartments)
+      .Select(ConvertLetterToPriority)
+      .Sum();
+  }
+
+  public static char FindCommonLetterInThreeRucksacks(string rucksack1, string rucksack2, string rucksack3)
+  {
+    return rucksack1.Intersect(rucksack2).Intersect(rucksack3).Single();
+  }
+
+  public static int FindSumOfPrioritiesOfThreeElfGroups(IEnumerable<string> rucksacks)
+  {
+    return rucksacks
+      .Select((rucksack, index) => (rucksack, index))
+      .GroupBy(tuple => tuple.index / 3)
+      .Select(group => FindCommonLetterInThreeRucksacks(group.ElementAt(0).rucksack, group.ElementAt(1).rucksack, group.ElementAt(2).rucksack))
       .Select(ConvertLetterToPriority)
       .Sum();
   }
