f05f1dbe330a2703dbb2441ccaf16f2836d66881
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2022-12-04 16:54:55 -0500

    Complete 04-CampCleanup-Part 2

--- solutions/04-CampCleanup/CampCleanupTests.cs
+++ solutions/04-CampCleanup/CampCleanupTests.cs
@@ -56,6 +56,60 @@ public class CampCleanupTests : IClassFixture<InputFilesFixture>
 
     actual.Should().Be(471);
   }
+
+  [Fact]
+  public void ShouldFind2PairsWhereOneRangeOverlapsTheOtherInTestInput()
+  {
+    int actual = _fixture.TestInput
+      .Select(line => line.Split(','))
+      .Select(pairs => new[] { pairs[0].Split('-'), pairs[1].Split('-') })
+      .Select(pairs => new[] { (int.Parse(pairs[0][0]), int.Parse(pairs[0][1])), (int.Parse(pairs[1][0]), int.Parse(pairs[1][1])) })
+      .Select(FoundOverlap)
+      .Sum();
+
+    static int FoundOverlap((int, int)[] pairs)
+    {
+      Array.Sort(pairs, Compare);
+      for (int i = 1; i < pairs.Length; i++)
+      {
+        if (pairs[i - 1].Item2 >= pairs[i].Item1) return 1;
+      }
+      return 0;
+    }
+
+    static int Compare((int, int) a, (int, int) b) {
+      return a.Item1 == b.Item1 ? b.Item2 - a.Item2 : a.Item1 - b.Item1;
+    };
+
+    actual.Should().Be(4);
+  }
+
+  [Fact]
+  public void ShouldFind2PairsWhereOneRangeOverlapsTheOtherInSolutionInput()
+  {
+    int actual = _fixture.SolutionInput
+      .Select(line => line.Split(','))
+      .Select(pairs => new[] { pairs[0].Split('-'), pairs[1].Split('-') })
+      .Select(pairs => new[] { (int.Parse(pairs[0][0]), int.Parse(pairs[0][1])), (int.Parse(pairs[1][0]), int.Parse(pairs[1][1])) })
+      .Select(FoundOverlap)
+      .Sum();
+
+    static int FoundOverlap((int, int)[] pairs)
+    {
+      Array.Sort(pairs, Compare);
+      for (int i = 1; i < pairs.Length; i++)
+      {
+        if (pairs[i - 1].Item2 >= pairs[i].Item1) return 1;
+      }
+      return 0;
+    }
+
+    static int Compare((int, int) a, (int, int) b) {
+      return a.Item1 == b.Item1 ? b.Item2 - a.Item2 : a.Item1 - b.Item1;
+    };
+
+    actual.Should().Be(888);
+  }
 }
 
 public class InputFilesFixture : IAsyncLifetime
